e8a63e879ce502ebd126026937644c6813fc175c
Muhammad Gibran Naufal Dzakwan <gindzak@gmail.com>
2025-04-17 09:23:22 +0700

    First Commit

---
+++ src/main/java/id/co/bsi/walled_2/Walled2Application.java
@@ -0,0 +1,13 @@
+package id.co.bsi.walled_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Walled2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Walled2Application.class, args);
+	}
+
+}
